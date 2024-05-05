75bf5e38d437b31281e4a1a169de8d89aa4b66fd
morinana <kinakorabbit1182@gmail.com>
2024-05-05 22:20:07 +0900

    [ADD]router.refeshの使い方を理解する

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import NotesLlist from './components/notes-list'
+import TimerCounter from './components/timer-counter'
+import { Suspense } from 'react'
+import Spinner from './components/spinner'
+import RefreshBtn from './components/refresh-btn'
 
 export default function Page() {
   return (
     <main>
       <div className="m-10 text-center">
         <p>Hello World😃</p>
-        {/* @ts-ignore */}
-        <NotesLlist />
+        <Suspense fallback={<Spinner color="border-green-500" />}>
+          {/* @ts-ignore */}
+          <NotesLlist />
+        </Suspense>
+        <TimerCounter />
+        <RefreshBtn />
       </div>
     </main>
   )
